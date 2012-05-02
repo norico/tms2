364db3a3c51672195334736046338be0ebca63c1
Norico <norico@tm-tnt.net>
2012-05-02 14:18:29 +0200

    Signed-off-by: Norico <norico@tm-tnt.net>

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ TrackMania Serveurs Scripts
 
 ## Commandes
  * start
+ Démarrage du serveur. 
  * stop
+ Arrêt du serveur.
  * restart
+ Redémarrage du serveur.
  * status
+ État du serveur.
  * create
+ Création de fichier nécessaire au démarrage du serveur.
--- tms2-login.sh
+++ tms2-login.sh
@@ -13,13 +13,12 @@ source ./${conf}/config
 source ./${conf}/functions
 
 case "$1" in 
-	start)		fn_do_start	;;
-	stop)		fn_do_stop	;;
+	start)		fn_do_start ;;
+	stop)		fn_do_stop ;;
 	restart)	fn_do_restart ;;
 	status)		fn_status ;;
 	create)		fn_do_create ;;
 	restart)	fn_do_restart ;;	
-	joke) 		fn_joke ;;
 	*)	clear
 		fn_version
 		echo -e "${_msg_usage}: $0 {start|stop|restart|status|create}"
